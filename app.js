09d9531abaeda3891378d945cc73f749c6c3fa45
ALentini222 <77400292+ALentini222@users.noreply.github.com>
2024-07-21 12:23:55 -0400

    port listener

--- app.js
+++ app.js
@@ -58,5 +58,11 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
 module.exports = app;
